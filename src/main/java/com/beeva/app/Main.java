be45997fc61d0cbf6d096d54e170b83c88946206
Antonio <antonio.com>
2017-04-24 12:54:34 -0500

    este es mi primer proyecto

---
+++ src/main/java/com/beeva/app/Main.java
@@ -0,0 +1,17 @@
+package com.beeva.app;
+
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+   public static void main(String[] args) {
+      ApplicationContext context = 
+             new ClassPathXmlApplicationContext("Beans.xml");
+
+      HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
+
+      obj.getMessage();
+   }
+}
